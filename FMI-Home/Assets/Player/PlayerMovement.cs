7457b4f641cf4e45338a64c5dccadd48ac720155
Petar Dobrev <peterdobrevxd@gmail.com>
2024-03-16 02:30:35 +0200

    Migrated from Netcode For GameObjects To Photon 2
    Changed the multiplayer solution from netcode for gameobjects to photon, because we are going to try to implement voice chat and the solution that photon offers is better.

--- FMI-Home/Assets/Player/PlayerMovement.cs
+++ FMI-Home/Assets/Player/PlayerMovement.cs
@@ -1,19 +1,28 @@
 using System;
 using Unity.Mathematics;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // private Vector3 _direction;
     private Vector3 _direction = Vector3.zero;
 
     [SerializeField]
     private float speed = .25f;
 
+    private PhotonView view;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
     private void Update()
     {
-        _direction.x = Input.GetAxisRaw("Horizontal");
-        _direction.y = Input.GetAxisRaw("Vertical");
+        if(view != null && view.IsMine) { 
+            _direction.x = Input.GetAxisRaw("Horizontal");
+            _direction.y = Input.GetAxisRaw("Vertical");
+        }
     }
 
     void FixedUpdate()
