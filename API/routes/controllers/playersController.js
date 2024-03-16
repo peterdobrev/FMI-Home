f38ed29fd099f2637b4c578da613a0ea6fccaf58
Slavi15 <slavirezashkiofficial@gmail.com>
2024-03-16 21:20:52 +0200

    Add api routing for player by ID query and createPlayer

--- API/routes/controllers/playersController.js
+++ API/routes/controllers/playersController.js
@@ -6,9 +6,30 @@ const getPlayers = async (req, res) => {
         res.status(200).send(products);
     } catch (err) {
         res.status(500).send({ message: err.message });
-    }
+    };
 };
 
+const getPlayerByID = async (req, res) => {
+    try {
+        const player = await Player.findById(req.params.playerID);
 
+        player ? 
+            res.status(200).json(player) :
+            res.status(400).json({ message: `Player with id: ${req.params.playerID} not found!` });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    };
+};
 
-module.exports = { getPlayers };
+const createPlayer = async (req, res) => {
+    const player = new Player(req.body);
+
+    try {
+        const newPlayer = await player.save();
+        res.status(201).json(newPlayer);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    };
+};
+
+module.exports = { getPlayers, getPlayerByID, createPlayer };
--- API/routes/index.js
+++ API/routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
-const { getPlayers } = require("./controllers/playersController.js");
+const { getPlayers, getPlayerByID, createPlayer } = require("./controllers/playersController.js");
 
 router.get("/players", getPlayers);
+router.get("/players/:playerID", getPlayerByID);
+router.post("/players", createPlayer);
 
 module.exports = router;
