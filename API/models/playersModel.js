95f29d9c786e9b7cc09eb772db49b51c3899ff60
Slavi15 <slavirezashkiofficial@gmail.com>
2024-03-16 20:44:53 +0200

    Add mongoose DB Schema Structure

---
+++ API/models/playersModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const Joigoose = require("joigoose")(mongoose);
+
+const joiPlayerSchema = Joi.object({
+    playerName: Joi.string().required(),
+    facultyNumber: Joi.string().required(),
+    points: Joi.number().integer()
+});
+
+const mongoosePlayerSchema = new mongoose.Schema(Joigoose.convert(joiPlayerSchema));
+const Player = mongoose.model("players", mongoosePlayerSchema, "players");
+module.exports = { Player };
