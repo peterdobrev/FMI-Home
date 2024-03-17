87cebafa31d99db7f228e250c0ac9fb4f8fd9b6f
Petar Dobrev <peterdobrevxd@gmail.com>
2024-03-17 02:58:14 +0200

    Added scenes to enter the game

---
+++ FMI-Home/Assets/SceneManager.cs
@@ -0,0 +1,12 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public void LoadSelection()
+    {
+        PhotonNetwork.LoadLevel("selectScene");
+    }
+}
